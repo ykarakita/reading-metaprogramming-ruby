5b62008d6c9a7dc4fe50676de2bf97078a43adcf
ykarakita <taf.749@gmail.com>
2021-04-29 11:47:13 +0900

    solved 00_setup

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,4 +5,32 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
+  def initialize(first_name, middle_name = '', last_name)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  attr_accessor :first_name
+
+  def full_name
+    [first_name, middle_name, last_name].reject(&:empty?).join(' ')
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    %w(first_name middle_name last_name).each do |name|
+      current_value = send(name)
+      send("#{name}=", current_value.upcase)
+    end
+    full_name
+  end
+
+  private
+
+  attr_accessor :middle_name, :last_name
 end
